5c237a179826cdc2b96d9cd99d0387479fc1c2d0
TakumiTakanoTMCIT <sakana0109taifuu@gmail.com>
2024-10-13 10:09:55 +0900

    豆のアニメーションが機能するようになった

--- Assets/Bullets/DamageAbleFinder.cs
+++ Assets/Bullets/DamageAbleFinder.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class DamageAbleFinder : MonoBehaviour
 {
+    Animator animator;
+    MameAnimator mamaeAnimator;
+
     [SerializeField] private int damageAmount = 1;
+
+    private void Awake()
+    {
+        animator = this.gameObject.MyGetComponent_NullChker<Animator>();
+        mamaeAnimator = new MameAnimator(animator);
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         IDamageableFromShot damageable = other.GetComponent<IDamageableFromShot>();
-        if (damageable != null)
-        {
-            damageable.TakeDamage(damageAmount);
-            this.gameObject.GetComponent<IDestroyable>().DestroyShell();
-        }
+
+        if (damageable == null)
+            return;
+        damageable.TakeDamage(damageAmount);
+        mamaeAnimator.TakeDamage();
     }
 }
--- Assets/ShellMainBodyCrtl.cs
+++ Assets/ShellMainBodyCrtl.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.Pool;
+using UnityEngine.Video;
 
-public class ShellMainBodyCrtl : MonoBehaviour,IDestroyable
+public class ShellMainBodyCrtl : MonoBehaviour, IDestroyable
 {
     Rigidbody2D rb;
     SpriteRenderer spriteRenderer;
     MameShellManager shellManager;
+    Animator animator;
 
     private ObjectPool<GameObject> pool;
 
@@ -13,20 +15,25 @@ public class ShellMainBodyCrtl : MonoBehaviour,IDestroyable
 
     float speed;
 
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-    }
+    bool isMoveable = true;
 
     /// <summary>
     /// InitはShellManagerでCreateShellしたときに呼び出されます
     /// </summary>
     public void Init(MameShellManager shellManager, ObjectPool<GameObject> pool, float speed)
     {
+        animator = this.gameObject.MyGetComponent_NullChker<Animator>();
+        rb = this.gameObject.MyGetComponent_NullChker<Rigidbody2D>();
+        spriteRenderer = this.gameObject.MyGetComponent_NullChker<SpriteRenderer>();
+
         this.shellManager = shellManager;
         this.pool = pool;
         this.speed = speed;
+    }
+
+    private void OnEnable()
+    {
+        isMoveable = true;
     }
 
     /// <summary>
@@ -35,10 +42,13 @@ public class ShellMainBodyCrtl : MonoBehaviour,IDestroyable
     public void SetDirection(bool direction)
     {
         this.direction = direction;
+        spriteRenderer.flipX = !direction;
     }
 
     private void FixedUpdate()
     {
+        if (!isMoveable) return;
+
         if (direction) rb.velocity = new Vector2(speed, 0);
         else rb.velocity = new Vector2(-speed, 0);
     }
@@ -51,5 +61,34 @@ public class ShellMainBodyCrtl : MonoBehaviour,IDestroyable
     public void DestroyShell()
     {
         pool.Release(this.gameObject);
+    }
+
+    public void StopMove()
+    {
+        isMoveable = false;
+        rb.velocity = Vector2.zero;
+    }
+}
+
+public class MameAnimator
+{
+    ShellMainBodyCrtl mainbody;
+    Animator animator;
+    public MameAnimator(Animator animator)
+    {
+        this.animator = animator;
+        mainbody = animator.gameObject.MyGetComponent_NullChker<ShellMainBodyCrtl>();
+    }
+
+    public void TakeDamage()
+    {
+        animator.SetTrigger("isTakeDamage");
+        mainbody.StopMove();
+        Debug.Log("SetTrigger");
+    }
+
+    public void RefrectShell()
+    {
+        //追記予定です。
     }
 }
